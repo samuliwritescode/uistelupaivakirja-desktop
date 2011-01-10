cf325ce2684ad285dfc3421fb5d1ec122a0cd172
Samuli Penttilä <cape@anteex.capeismi.fi>
2011-01-10 20:18:07 +0200

    -kalan tiedot syötetään omassa panelissaan.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,13 +24,13 @@ MainWindow::MainWindow(QWidget *parent) :
         kalat->insertItem(0, "hauki");
         kalat->insertItem(0, "ahven");
         draggable->setAcceptDrops(true);
-        ui->catchGrid->addWidget(kalat, loop, 0);
+      /*  ui->catchGrid->addWidget(kalat, loop, 0);
         ui->catchGrid->addWidget(new QLineEdit("6kg"), loop, 1);
         ui->catchGrid->addWidget(new QLineEdit("85cm"), loop, 2);
         ui->catchGrid->addWidget(new QLineEdit("22C"), loop, 3);
         ui->catchGrid->addWidget(new QLineEdit("5m"), loop, 4);
         ui->catchGrid->addWidget(draggable, loop, 5);
-        ui->catchGrid->addWidget(poi, loop, 6);
+        ui->catchGrid->addWidget(poi, loop, 6);*/
     }
 
     //ui->lureList->setIconSize(QSize(100,50));
